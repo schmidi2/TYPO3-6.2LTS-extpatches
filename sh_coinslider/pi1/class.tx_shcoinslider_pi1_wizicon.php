df6689384e3ded542650f2e90a17a1140e17220b
schmidi2 <schmidi2@directbox.com>
2014-08-15 12:53:11 +0200

    Fatal error: Call to undefined method TYPO3\CMS\Core\Utility\GeneralUtility::readLLXMLfile() in ... typo3conf/ext/sh_coinslider/pi1/class.tx_shcoinslider_pi1_wizicon.php on line 70

--- sh_coinslider/pi1/class.tx_shcoinslider_pi1_wizicon.php
+++ sh_coinslider/pi1/class.tx_shcoinslider_pi1_wizicon.php
@@ -67,7 +67,7 @@ class tx_shcoinslider_pi1_wizicon {
 					 */
 					function includeLocalLang()	{
 						$llFile = t3lib_extMgm::extPath('sh_coinslider').'locallang.xml';
-						$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
+						$LOCAL_LANG = \TYPO3\CMS\Core\Utility\GeneralUtility::readLLfile($llFile, $GLOBALS['LANG']->lang);
 
 						return $LOCAL_LANG;
 					}
