b704017e69c7128e2d488481b07174c44f0892dd
schmidi2 <schmidi2@directbox.com>
2014-08-19 13:42:07 +0200

    Extension key	stever_rsscontent Version	1.0.2 Stable Last updated	May 14, 2007 First upload	February 8, 2006 Downloads	2,689 Category	 Frontend Plugins Dependencies	 none Conflicts	 none Manual	Extension Manual Interested in code?	Check quality Username	 irishflute Author	 Steve Ryan

---
+++ stever_rsscontent-1.0.2/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "stever_rsscontent"
+#
+# Auto generated 09-04-2010 16:14
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'RSS content',
+	'description' => 'creates an RSS feed from latest updates to the tt_content table',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.2',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Steve Ryan',
+	'author_email' => 'stever@syntithenai.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '3.0.0-0.0.0',
+			'typo3' => '3.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"7967";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"8aea";s:14:"ext_tables.php";s:4:"d087";s:28:"ext_typoscript_constants.txt";s:4:"88b6";s:24:"ext_typoscript_setup.txt";s:4:"b798";s:16:"locallang_db.php";s:4:"707f";s:37:"pi1/class.tx_steverrsscontent_pi1.php";s:4:"6a2c";s:17:"pi1/locallang.php";s:4:"41bd";s:24:"pi1/static/editorcfg.txt";s:4:"1062";s:20:"pi1/static/setup.txt";s:4:"fe49";s:14:"doc/manual.sxw";s:4:"1046";s:19:"doc/wizard_form.dat";s:4:"130f";s:20:"doc/wizard_form.html";s:4:"cb5f";}',
+);
+
+?>
---
+++ stever_rsscontent-1.0.2/locallang_db.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Language labels for database tables/fields belonging to extension "stever_rsscontent"
+ *
+ * This file is detected by the translation tool.
+ */
+
+$LOCAL_LANG = Array (
+	'default' => Array (
+		'tt_content.list_type_pi1' => 'RSS feed of content',	
+	),
+);
+?>
